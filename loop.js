38927eb79e41ceed1d85a1df1b86a5c456df1b31
Habibur Rahman <habibur88454@gmail.com>
2024-09-03 23:47:23 +0600

    apply for while and for of loping

---
+++ loop.js
@@ -0,0 +1,19 @@
+const friends = ['jamal', 'kamal', 'josim', 'kodu'];
+
+for (const friend of friends) {
+    // console.log(friend)
+}
+
+// for loop
+for (let i = 0; i < friends.length; i++){
+    // console.log(i)
+    // console.log(friends[i])
+}
+
+
+// while
+let i = 0;
+while(i<friends.length){
+    console.log(friends[i])
+    i++
+}
