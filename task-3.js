6e11b68f0e80bb8d349f1cebebda2ccfc159ae8b
Habibur Rahman <habibur88454@gmail.com>
2024-09-04 01:36:22 +0600

    another task solved in different way

--- task-3.js
+++ task-3.js
@@ -1,2 +1,16 @@
 // Use a for...of loop to concatenate all the elements of an array
 //  into a single string.
+var names = ['Tom', 'Tim', 'Tin', 'Tik']
+const divided = names.join('')
+// console.log(divided)
+
+// Reverse the words of a sentence. Only the position of the word
+//  will be reversed. check out the output
+const statement = 'I am a hard working person';
+const reverseWord = statement.split(' ');
+const storeSentence = [];
+for (const word of reverseWord) {
+    storeSentence.unshift(word);
+}
+const sentenceResult = storeSentence.join(' ');
+console.log( sentenceResult)
